5af274f4bc64fe8abcc47bea2dccc958ef08b96f
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-01-19 14:33:45 +0330

    Fixed spotify api scopes bug

--- src/spotify.js
+++ src/spotify.js
@@ -3,7 +3,7 @@ const redirectUri = "http://localhost:3000/";
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 const scopes = [
-  "user-read-current-playing",
+  "user-read-currently-playing",
   "user-read-playback-state",
   "user-read-recently-played",
   "user-top-read",
