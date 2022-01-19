5b413018d9b21c6e514ae84c12ce2097815d2e5b
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-01-19 19:22:57 +0330

    exported access token from url and showing login/loggedin based on that token

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
+//helper
+import { getURLHashToken } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    let _token = getURLHashToken().access_token;
+    window.location.hash = "";
+    if (_token) setToken(_token);
+  }, []);
+
+  return <div className="app">{token ? <h2>Logged In</h2> : <Login />}</div>;
 }
 
 export default App;
