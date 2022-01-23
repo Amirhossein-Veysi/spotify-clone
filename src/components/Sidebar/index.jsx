32993b55aaa8d27b003eaba2dd5999a1b00db0de
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-01-23 20:21:50 +0330

    Updated sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Sidebar.css";
+//Logo
+import Logo from "../../img/spotify-green-logo.png";
+//components
+import SidebarOption from "../SidebarOption";
+//icons
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 function Sidebar() {
-  return <div className="sidebar">I am sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img src={Logo} alt="Spotify Logo" className="sidebar-logo" />
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Library" Icon={LibraryMusicIcon} />
+
+      <strong className="sidebar-title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOption title="Rock" />
+      <SidebarOption title="R&B" />
+    </div>
+  );
 }
 
 export default Sidebar;
---
+++ src/components/SidebarOption/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//css
+import "./SidebarOption.css";
+
+function SidebarOption({ title, Icon }) {
+  return (
+    <div className="sidebar-option">
+      {Icon && <Icon className="sidebar-icon" />}
+      {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+    </div>
+  );
+}
+
+export default SidebarOption;
