4697836f019e7cbf89b328a5b34d5125f0cf9dc9
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-01-26 23:35:41 +0330

    Updated banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//css
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img src="" alt="" />
+      <div className="info">
+        <p className="strong">PLAYLIST</p>
+        <h2>Discover Weekly</h2>
+        <p>description...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
